48b0bb25667c30a69a0a0ec0a86d8853a272bbe9
Jafnak <jafnakusman@gmail.com>
2024-09-08 10:11:00 +0530

    created

--- app.js
+++ app.js
@@ -2,21 +2,22 @@ const mongoose = require("mongoose")
 const bcrypt = require("bcrypt")
 const express = require("express")
 const jwt = require("jsonwebtoken")
-const  cors = require("cors")
+const cors = require("cors")
 const { elderModel } = require("./models/user")
+const { aloginModel } = require("./models/admin")
 
-const app=express()
+const app = express()
 app.use(cors())
 app.use(express.json())
 
 mongoose.connect("mongodb+srv://Jafna02:jafna9074@cluster0.icijy.mongodb.net/ElderlinkDb?retryWrites=true&w=majority&appName=Cluster0")
 
-const generateHashedPassword = async(password) =>{
+const generateHashedPassword = async (password) => {
     const salt = await bcrypt.genSalt(10)  //salt=cost factor value
-    return bcrypt.hash(password,salt)
+    return bcrypt.hash(password, salt)
 }
 
-app.post("/signUp",async(req,res)=>{
+app.post("/signUp", async (req, res) => {
 
     let input = req.body
     let hashedPassword = await generateHashedPassword(input.password)
@@ -26,43 +27,90 @@ app.post("/signUp",async(req,res)=>{
     let user = new elderModel(input)
     user.save()
     console.log(user)
-    res.json({"status":"success"})
+    res.json({ "status": "success" })
 })
 
 
 
-app.post("/signin",(req,res)=>{
+app.post("/signin", (req, res) => {
     let input = req.body
-    elderModel.find({"email":req.body.email}).then(
-        (response)=>{
+    aloginModel.find({ "email": req.body.email }).then(
+        (response) => {
             if (response.length > 0) {
                 let dbPassword = response[0].password  //entered email is compared with existing password(email)
                 console.log(dbPassword)
-                bcrypt.compare(input.password,dbPassword,(error,isMatch)=>{ //input pswd and hashed pswd is  compared
+                bcrypt.compare(input.password, dbPassword, (error, isMatch) => { //input pswd and hashed pswd is  compared
                     if (isMatch) {
                         //if login success generate token
-                        jwt.sign({email:input.email},"elder-app",{expiresIn:"1d"},
-                            (error,token)=>{
+                        jwt.sign({ email: input.email }, "elder-app", { expiresIn: "1d" },
+                            (error, token) => {
                                 if (error) {
-                                    res.json({"status":"unable to create token"})
+                                    res.json({ "status": "unable to create token" })
                                 } else {
-                                    res.json({"status":"success","userId":response[0]._id,"token":token})
+                                    res.json({ "status": "success", "userId": response[0]._id, "token": token })
                                 }
                             }
                         )
                     } else {
-                        res.json({"status":"incorrect"})
+                        res.json({ "status": "incorrect" })
                     }
                 })
-                
+
             } else {
-                res.json({"status":"user not found"})
+                res.json({ "status": "user not found" })
             }
         }
     ).catch()
 })
 
 
-app.listen(8080,()=>{
+
+app.post("/adminlogin", (req, res) => {
+    let input = req.body;
+
+    // Default admin credentials
+    const adminEmail = 'admin@gmail.com';
+    const adminPassword = 'admin123';
+
+    // Check if the input matches admin credentials
+    if (input.email === adminEmail && input.password === adminPassword) {
+        // Admin login successful
+        jwt.sign({ email: input.email }, "elder-app", { expiresIn: "1d" }, (error, token) => {
+            if (error) {
+                res.json({ "status": "Token credentials failed" });
+            } else {
+                res.json({ "status": "success", "token": token, "message": "Admin logged in successfully" });
+            }
+        });
+    } else {
+        // Check if the user exists in the database
+        aloginModel.find({ name: input.name }).then((response) => {
+            if (response.length > 0) {
+                const validator = bcrypt.compareSync(input.password, response[0].password);
+                if (validator) {
+                    // User login successful
+                    jwt.sign({ email: input.email}, "elder-app", { expiresIn: "1d" }, (error, token) => {
+                        if (error) {
+                            res.json({ "status": "Token credentials failed" });
+                        } else {
+                            res.json({ "status": "success", "token": token });
+                        }
+                    });
+                } else {
+                    res.json({ "status": "Wrong password" });
+                }
+            } else {
+                res.json({ "status": "Username doesn't exist" });
+            }
+        }).catch((err) => {
+            res.json({ "status": "Error occurred", "error": err.message });
+        });
+    }
+});
+
+
+
+
+app.listen(8080, () => {
     console.log("server started")
 })
